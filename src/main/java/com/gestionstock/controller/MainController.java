7d49db817da358b97afa5879db0bef6167b5800b
rachidatou <rachidatouyounga@gmail.com>
2026-08-20 12:09:45 +0100

    feat: bouton Deconnexion + affichage conditionnel selon le role ADMIN/GESTIONNAIRE

--- src/main/java/com/gestionstock/controller/MainController.java
+++ src/main/java/com/gestionstock/controller/MainController.java
@@ -3,8 +3,13 @@ package com.gestionstock.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import com.gestionstock.util.SessionUtilisateur;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 /*
     -@FXML: Annotation qui connecte un attribut Java à un composant déclaré dans le fichier XML via son fx:id
@@ -13,14 +18,30 @@ import javafx.scene.layout.StackPane;
 public class MainController {
     @FXML
     private StackPane contenuPrincipale;
+    @FXML
+    private Button btnDeconnexion;
+    @FXML
+    private Button btnGestionCompte;
+
 
     @FXML
-    public void initialize() { afficherDashboard();}
+    public void initialize() {
+        if(SessionUtilisateur.estAdmin()){
+            btnGestionCompte.setVisible(true);
+
+        }else {
+            //setVisible(false) propriete java qui cache un boutton
+            btnGestionCompte.setVisible(false);
+            //manager reorganiser liberer espace
+            btnGestionCompte.setManaged(false);
+        }
+        afficherDashboard();}
 
     @FXML
     private void afficherDashboard() {
         contenuPrincipale.getChildren().clear();
         contenuPrincipale.getChildren().add(new Label("Dashboard"));
+
     }
 
     @FXML
@@ -50,4 +71,29 @@ public class MainController {
             e.printStackTrace();
         }
     }
+    @FXML
+    private void seDeconnecter() {
+        SessionUtilisateur.deconnecter();
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/gestionstock/LoginView.fxml")
+            );
+            Scene scene = new Scene(loader.load());
+            scene.getStylesheets().add(
+                    getClass().getResource("/com/gestionstock/style.css").toExternalForm()
+            );
+
+            Stage stage = (Stage) btnDeconnexion.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Gestion Stock IAGE");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @FXML
+    private void gererComptes() {
+        System.out.println("");
+    }
+
 }
